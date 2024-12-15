5ad5ba726b876691bcdcc55df39722bf9abf7920
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2024-12-15 15:11:12 +0000

    portals now teleport only from one side

--- portal.gd
+++ portal.gd
@@ -1,19 +1,22 @@
 extends Node
 
 var player_camera: Camera3D
+var portal_entry: MeshInstance3D
+var portal_exit: MeshInstance3D
 var portal_subviewport: SubViewport
 var portal_camera: Camera3D
 var dummy_camera_entry: Node3D
 var dummy_body_entry: Node3D
 var dummy_camera_exit: Node3D
 var dummy_body_exit: Node3D
+var portal_travellers: Dictionary
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# Get existing nodes
-	var portal_entry: MeshInstance3D = $Entry
-	var portal_exit: MeshInstance3D = $Exit
+	portal_entry = $Entry
+	portal_exit = $Exit
 	
 	# Setup viewport
 	portal_subviewport = SubViewport.new()
@@ -59,27 +62,52 @@ func _ready() -> void:
 	hitboxArea.global_transform = portal_entry.global_transform
 	var hitboxCollision: CollisionShape3D = CollisionShape3D.new()
 	hitboxCollision.shape = BoxShape3D.new()
-	hitboxCollision.shape.size = Vector3(portal_entry.get_mesh().size.x, portal_entry.get_mesh().size.y, 2)
+	hitboxCollision.shape.size = Vector3(portal_entry.get_mesh().size.x, portal_entry.get_mesh().size.y, 0.1)
 	hitboxArea.add_child(hitboxCollision)
 	hitboxArea.body_entered.connect(_on_entry_body_entered)
+	hitboxArea.body_exited.connect(_on_entry_body_exited)
+	portal_travellers = {}
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	move_camera()
+	for body in portal_travellers.keys():
+		print(side_of_portal(body))
+		if portal_travellers[body] > side_of_portal(body):
+			teleport(body)
+			portal_travellers.erase(body)
+
+
+func move_camera() -> void:
 	dummy_camera_entry.set_global_transform(player_camera.get_global_transform())
 	dummy_camera_exit.set_transform(dummy_camera_entry.get_transform())
 	portal_camera.set_global_transform(dummy_camera_exit.get_global_transform())
 
 
-func _on_entry_body_entered(body: Node3D) -> void:
-	if body is not CharacterBody3D:
-		return
-	
+func teleport(body: Node3D) -> void:
 	dummy_body_entry.set_global_transform(body.get_global_transform())
 	dummy_body_exit.set_transform(dummy_body_entry.get_transform())
 	body.set_global_position(dummy_body_exit.get_global_position())
 	body.set_global_rotation(dummy_body_exit.get_global_rotation())
 
 
-func resize():
+func side_of_portal(body: Node3D) -> float:
+	var distance = body.global_position - portal_entry.global_position
+	return -portal_entry.basis.z.dot(distance)
+
+
+func resize() -> void:
 	portal_subviewport.size = get_viewport().size
+
+
+func _on_entry_body_entered(body: Node3D) -> void:
+	if body is not CharacterBody3D:
+		return
+	portal_travellers[body] = side_of_portal(body)
+
+
+func _on_entry_body_exited(body: Node3D) -> void:
+	if body is not CharacterBody3D:
+		return
+	portal_travellers.erase(body)
