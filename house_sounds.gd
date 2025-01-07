fec69a6c889646f05b8bc7eb8f276865ee1c4671
danielsilva12UA <104782036+danielsilva12UA@users.noreply.github.com>
2025-01-07 13:45:55 +0000

    fixed scripts

--- house_sounds.gd
+++ house_sounds.gd
@@ -6,13 +6,17 @@ extends Area3D
 var is_playing = false
 
 # This function is triggered when a body enters the area
-func _on_body_entered(body: CharacterBody3D) -> void:
+func _on_body_entered(body: Node3D) -> void:
+	if body is not CharacterBody3D:
+		return
 	if not is_playing:  # Only change song if it's not already playing
 		is_playing = true
 		main_audio_player.bgm_player("inhouse_song")  # Play the "natives_song" when entered
 
 # This function is triggered when a body exits the area
-func _on_body_exited(body: CharacterBody3D) -> void:
+func _on_body_exited(body: Node3D) -> void:
+	if body is not CharacterBody3D:
+		return
 	if is_playing:  # Only switch to default song if it was playing
 		is_playing = false
 		main_audio_player.bgm_player("default_song")  # Switch to "default_song" when exited
